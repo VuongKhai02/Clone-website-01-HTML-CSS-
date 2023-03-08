8279e9ac3e4e9b662b133501c85fea36a2a4f15b
VuongKhai02 <khaiitnoi@gmail.com>
2023-03-08 08:59:50 +0700

    first commit

---
+++ js/index.js
@@ -0,0 +1,61 @@
+
+/*
+let product = [
+    {
+        id: 1,
+        name: 'Cây kim tiền',
+        price: 1.111,
+        image: "../images/plant1.png"
+    },
+    {
+        id: 2,
+        name: 'Cây kim tiền 2',
+        price: 1.111,
+        image: '../images/plant1.png'
+    },{
+        id: 3,
+        name: 'Cây kim tiền 3',
+        price: 1.111,
+        image: '../images/plant1.png'
+    },
+    {
+        id: 4,
+        name: 'Cây kim tiền 4',
+        price: 1.111,
+        image: '../images/plant1.png'
+    },
+    {
+        id: 5,
+        name: 'Cây kim tiền 5',
+        price: 1.111,
+        image: '../images/plant1.png'
+    }
+]
+
+let viewProduct = '';
+for(let i = 0;i < product.length; i++){
+    let eachProduct = product[i];
+
+    let row = `<div class="card">
+                    <div class="img">
+                        <div class="desc">
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque aut vitae, voluptate qui assumenda ad debitis numquam similique eligendi excepturi</p>
+
+                            <div class="desc_button">
+                                <a href="#">Chi tiết</a>
+                                <a href="#">Mua ngay</a>
+
+                            </div>
+                        </div>
+                        <img src="${eachProduct.image}" alt="Cây kim tiền">
+                    </div>
+                    <div class="content">
+                        <a href="">${eachProduct.name}</a>
+                        <p>${eachProduct.price}<sup>đ</sup> </p>
+                    </div>
+                </div>`
+    viewProduct += row;
+}
+$('.row').html(viewProduct);
+
+*/
